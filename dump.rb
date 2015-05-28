c2289ac5350e80c932e3290ed66a4bb9f2d8cf14
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-05-28 06:01:22 -0400

    finish

--- dump.rb
+++ dump.rb
@@ -17,18 +17,36 @@ class Dump
 	def live
 		loop do
 			prompt
+			output
+			complete
 		end
 	end
 	
 	def prompt
 		# sets the main context for the app
-		puts "Select a master folder"; @folders[:master] = gets
+		puts "Select a master folder"; @folders[:master] = gets.strip
 		# chooses the subfolder to be used or created if needed
-		puts "Select a subfolder"; @folders[:sub] = gets
+		puts "Select a subfolder"; @folders[:sub] = gets.strip
 		# gets the group of pictures to be sorted
 		puts "Select some pictures (separate by comma, ', ')"
 		pictures = gets; num = 1; pictures.split(", ").each do |pic|
-			@pictures["pic_#{num}".to_sym] = pic; num += 1
+			@pictures["pic_#{num}".to_sym] = pic.strip; num += 1
+		end
+	end
+	
+	def output
+		puts @folders
+		puts @pictures
+	end
+	
+	def complete
+		puts "Finish?"
+		answer = gets.strip
+		case answer
+			when "Y", "Yes", "yes", "yeah", "yay", "y"
+				exit
+			when "N", "No", "no", "nay", "nah", "na", "n"
+				puts "\n\n"
 		end
 	end
 end
